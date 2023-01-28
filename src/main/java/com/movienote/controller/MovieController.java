20affb32bde31c136ecad2e5caf13b3f7dc2767a
Nadya Penkrat <nadya.penkrat@gmail.com>
2023-01-28 18:17:30 +0300

    add Movie Controller

---
+++ src/main/java/com/movienote/controller/MovieController.java
@@ -0,0 +1,55 @@
+package com.movienote.controller;
+
+import com.movienote.model.Movie;
+import com.movienote.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/movie")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping()
+    public List<Movie> getAllMovies() {
+
+        return movieService.getAll();
+
+    }
+
+    @GetMapping("/search")
+    public Movie getMovie(@RequestParam("title") String title) {
+
+        return movieService.getByTitle(title);
+
+    }
+
+    @PostMapping
+    public void saveMovie(@RequestBody Movie movie) {
+
+        movieService.save(movie);
+
+    }
+
+    @PutMapping()
+    public Movie updateMovie(@RequestBody Movie movie) {
+
+        return movieService.change(movie);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMovie(@PathVariable("id") Long id) {
+
+        movieService.delete(id);
+
+    }
+}
+
+
