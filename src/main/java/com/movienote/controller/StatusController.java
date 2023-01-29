79afc29e4fc19b3de3a8c78d1c682663f8d3e951
Nadya Penkrat <nadya.penkrat@gmail.com>
2023-01-29 17:14:33 +0300

    add Status Controller

---
+++ src/main/java/com/movienote/controller/StatusController.java
@@ -0,0 +1,31 @@
+package com.movienote.controller;
+
+import com.movienote.model.Status;
+import com.movienote.service.StatusService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/status")
+public class StatusController {
+
+    private final StatusService statusService;
+
+    public StatusController(StatusService statusService) {
+        this.statusService = statusService;
+    }
+
+    @GetMapping()
+    public List<Status> getAllStatuses() {
+
+        return statusService.getAll();
+    }
+
+}
+
+
+
