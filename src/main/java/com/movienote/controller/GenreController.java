e6b9c5aead5db55662c4abe03612f41874263599
Nadya Penkrat <nadya.penkrat@gmail.com>
2023-01-30 18:42:05 +0300

    add Genre Controller

---
+++ src/main/java/com/movienote/controller/GenreController.java
@@ -0,0 +1,53 @@
+package com.movienote.controller;
+
+import com.movienote.model.Genre;
+import com.movienote.service.GenreService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/genre")
+public class GenreController {
+
+    private final GenreService genreService;
+
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    @GetMapping()
+    public List<Genre> getAllGenres() {
+
+        return genreService.getAll();
+    }
+
+    @GetMapping("/{name}")
+    public Genre getGenre(@PathVariable("name") String name) {
+
+        return genreService.getByName(name);
+    }
+
+  @PostMapping()
+    public void saveGenre(@RequestBody Genre genre ){
+
+        genreService.save(genre);
+    }
+
+    @PutMapping()
+    public Genre updateGenre(@RequestBody Genre genre ) {
+
+        return genreService.change(genre);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteGenre(@PathVariable("id") Long id) {
+
+        genreService.delete(id);
+
+    }
+}
+
+
+
